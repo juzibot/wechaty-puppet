2148614178f7ccbbae5b682b96d89741ac97603a
NickWang <brotherstyx@gmail.com>
2022-12-07 13:33:42 +0800

    feat: 🎸 event room announce

--- src/schemas/event.ts
+++ src/schemas/event.ts
@@ -74,6 +74,14 @@ export interface EventRoomTopicPayload {
   timestamp : number,
 }
 
+export interface EventRoomAnnouncePayload {
+  changerId: string,
+  newAnnounce: string,
+  oldAnnounce?: string,
+  roomId: string,
+  timestamp: number
+}
+
 export interface EventScanPayload {
   status: ScanStatus,
 
